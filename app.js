f7dbe1c07c0c66242e18c1dec8800423bbaceb78
Sohazur <mdsohazurislam@gmail.com>
2021-08-20 04:35:41 +0600

    functional plus minus button on items count

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('I am in a separate file')
+function addition(itemSerial, itemNum) {
+  document.getElementById(itemSerial).addEventListener("click", () => {
+    document.getElementById(itemNum).value =
+      parseInt(document.getElementById(itemNum).value) + 1;
+  });
+}
+
+function substraction(itemSerial, itemNum) {
+  document.getElementById(itemSerial).addEventListener("click", () => {
+    if (parseInt(document.getElementById(itemNum).value) > 0) {
+      document.getElementById(itemNum).value =
+        parseInt(document.getElementById(itemNum).value) - 1;
+    }
+  });
+}
+
+addition("plusNum1", "itemNum1");
+addition("plusNum2", "itemNum2");
+substraction("minusNum1", "itemNum1");
+substraction("minusNum2", "itemNum2");
